7d8cff2a5b748247556a65c56b364fbfd136b78b
cosmovane <vanessa.cnr97@hotmail.com>
2024-03-09 21:09:20 -0600

    ✨ create constants file and use them in starData

---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+export const BASE_STAR_RADIUS = 2; // Let's consider this the minimum size for a star so they're visible in the canvas
+
+// A star color is given by it's surface temperature. In this contant colors are given by their temperature in Kelvin
+export const COLORS = {
+  K14087: 'rgb(174,192,256)',
+  BASE: 'yellow',
+};
--- src/data/data.js
+++ src/data/data.js
@@ -1,28 +1,120 @@
+import { BASE_STAR_RADIUS, COLORS } from '../constants/constants';
+
 // Source: https://theskylive.com/sky/constellations/orion-bright-stars
 
 export const starData = [
-  { x: 240, y: 375, radius: 4, color: 'yellow', name: 'Betelgeuse' }, // Hombro izquierdo
-  { x: 370, y: 275, radius: 4, color: 'yellow', name: 'Meissa' }, // Cuello
+  {
+    x: 240,
+    y: 375,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Betelgeuse',
+  }, // Hombro izquierdo
+  {
+    x: 370,
+    y: 275,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Meissa',
+  }, // Cuello
   {
     x: 510,
     y: 375,
-    radius: 4,
-    color: 'yellow',
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
     name: 'Bellatrix',
   }, // Hombro derecho
-  { x: 370, y: 625, radius: 4, color: 'yellow', name: 'Alnitak' }, // Estrella izquierda del cinturon
-  { x: 420, y: 595, radius: 4, color: 'yellow', name: 'Alnilam' }, // Estrella central del cinturon
-  { x: 480, y: 555, radius: 4, color: 'yellow', name: 'Mintaka' }, // Estrella derecha del cinturon
-  { x: 330, y: 825, radius: 4, color: 'yellow', name: 'Shaiph' }, // Pie izquierdo
-  { x: 600, y: 755, radius: 4, color: 'yellow', name: 'Rigel' }, // Pie derecho
-  { x: 750, y: 355, radius: 4, color: 'yellow', name: 'Pi-3 Orionis' }, // Estrella central del escudo
-  { x: 730, y: 255, radius: 4, color: 'yellow', name: 'Omicron-2 Orionis' }, // Estrella superior del escudo
-  { x: 730, y: 455, radius: 4, color: 'yellow', name: 'Pi-5 Orionis' }, // Estrella inferior del escudo
-  { x: 125, y: 300, radius: 4, color: 'yellow', name: 'Mu Orionis' }, // Codo
-  { x: 70, y: 190, radius: 4, color: 'yellow', name: 'Xi Orionis' }, // Mano
-  { x: 95, y: 175, radius: 4, color: 'yellow', name: 'Nu Orionis' }, // Mano
-  { x: 110, y: 45, radius: 4, color: 'yellow', name: 'Chi-1 Orionis' }, // Espada
-  { x: 160, y: 35, radius: 4, color: 'yellow', name: 'Chi-2 Orionis' }, // Espada
+  {
+    x: 370,
+    y: 625,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Alnitak',
+  }, // Estrella izquierda del cinturon
+  {
+    x: 420,
+    y: 595,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Alnilam',
+  }, // Estrella central del cinturon
+  {
+    x: 480,
+    y: 555,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Mintaka',
+  }, // Estrella derecha del cinturon
+  {
+    x: 330,
+    y: 825,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Shaiph',
+  }, // Pie izquierdo
+  {
+    x: 600,
+    y: 755,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Rigel',
+  }, // Pie derecho
+  {
+    x: 750,
+    y: 355,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Tabit',
+  }, // Estrella central del escudo
+  {
+    x: 730,
+    y: 255,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Omicron-2 Orionis',
+  }, // Estrella superior del escudo
+  {
+    x: 730,
+    y: 455,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Pi-5 Orionis',
+  }, // Estrella inferior del escudo
+  {
+    x: 125,
+    y: 300,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Mu Orionis',
+  }, // Codo
+  {
+    x: 70,
+    y: 190,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Xi Orionis',
+  }, // Mano
+  {
+    x: 95,
+    y: 175,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Nu Orionis',
+  }, // Mano
+  {
+    x: 110,
+    y: 45,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Chi-1 Orionis',
+  }, // Espada
+  {
+    x: 160,
+    y: 35,
+    radius: BASE_STAR_RADIUS,
+    color: COLORS.BASE,
+    name: 'Chi-2 Orionis',
+  }, // Espada
 ];
 
 export const lineData = [
